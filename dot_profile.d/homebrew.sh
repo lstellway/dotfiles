1987ab351a8f614f950b00a6a248564d240adc9e
lstellway <lstellway@users.noreply.github.com>
2023-10-04 20:57:46 -0700

    Adds helper function for building brew package source dir path

--- dot_profile.d/homebrew.sh
+++ dot_profile.d/homebrew.sh
@@ -1,2 +1,15 @@
 [ -f "/opt/homebrew/bin/brew" ] && eval "$(/opt/homebrew/bin/brew shellenv)"
 
+brew-dir() {
+  [ -n "${1}" ] || (echo "No package provided" && exit 1)
+
+  PKG="${1}"
+  PKG_ROOT=$(brew --cellar "${PKG}")
+
+  [ -d "${PKG_ROOT}" ] || (echo "Package '${PKG}' is not installed" && exit 1)
+
+  PKG_VERSION=$(brew info --json "${PKG}" | jq -r '.[0].installed[0].version')
+  [ -d "${PKG_ROOT}/${PKG_VERSION}" ] || (echo "Package '${PKG}' could not be found" && exit 1)
+
+  printf "%s/%s" "${PKG_ROOT}" "${PKG_VERSION}"
+}
