989d766196e746818792dab5c09cef067affd09c
lstellway <lstellway@users.noreply.github.com>
2023-10-09 14:34:08 -0700

    Adds helper for looking up words

--- dot_profile.d/aliases.sh
+++ dot_profile.d/aliases.sh
@@ -23,6 +23,10 @@ empty() {
   done
 }
 
+dictionary () {
+  curl dict://dict.org/d:"${1}" | ${PAGER:-less}
+}
+
 dotenv() {
   if [ -f ".env" ]; then
     export $(cat .env | sed 's/ = /=/g' | sed 's/^#.*//g' | xargs)
