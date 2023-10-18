bd9c25dbf86415911f4e48e81a47e081d063c2f5
lstellway <lstellway@users.noreply.github.com>
2023-10-18 14:21:50 -0700

    Adds helper to connect to rootless

--- dot_profile.d/docker.sh
+++ dot_profile.d/docker.sh
@@ -1,6 +1,7 @@
 # Create a docker context to make use of this
 # docker context create remote --description "Remote host" --docker "host=unix://${HOME}/.docker/remote.sock"
 
+# Connect to a remote Docker host
 docker-remote() {
   local HOST="${1}"
   [ -n "${HOST}" ] || (echo "A host must be provided" && exit 1)
@@ -11,5 +12,16 @@ docker-remote() {
   local REMOTE_SOCKET="${2:-/var/run/docker.sock}"
   ssh -L ${DEST_PATH}:${REMOTE_SOCKET} -N ${HOST}
   rm -f ${DEST_PATH}
+}
+
+# Connect to a remote Docker host deployed with rootless
+docker-remote-rootless() {
+  local HOST="${1}"
+  [ -n "${HOST}" ] || (echo "A host must be provided" && exit 1)
+  
+  local REMOTE_UID=$(ssh ${HOST} id -u)
+  REMOTE_SOCKET="/var/run/user/${REMOTE_UID}/docker.sock"
+
+  docker-remote ${HOST} ${REMOTE_SOCKET}
 }
 
