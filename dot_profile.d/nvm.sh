ad050cda51f72bee845d26fb69fa31e7f4501e34
lstellway <lstellway@users.noreply.github.com>
2023-10-28 10:23:34 -0700

    Add bootstrapping for nvm

--- dot_profile.d/nvm.sh
+++ dot_profile.d/nvm.sh
@@ -1,3 +1,10 @@
 # NVM
 [ -f ~/.nvmrc ] && source ~/.nvmrc
 
+# Creates .nvm Directory
+[ -d ~/.nvm ] ||  mkdir ~/.nvm
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
